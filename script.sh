864c7cdd4816352b11d224a320905a303536fbbb
GabrielaKubo <gabriela.kubo@sptech.school>
2022-11-29 15:24:37 -0300

    script GUI

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Criando jar executável
+if [ $? -eq 0 ];
+then
+	echo "Arquivo .jar não instalado!"
+	echo "Gostaria de instalar arquivo .jar Gerencie! ? (s/n)"
+	read inst
+	if [ \"$inst\" == \"s\" ];
+	then
+		cd /home/ubuntu/Desktop
+		git clone https://github.com/Gerencie-Monitoramento-de-totens/JAR.git
+		echo "Arquivo clonado com sucesso!"
+		echo "Executando arquivo"
+		cd JAR/gerencie/target
+		java -jar gerencie-1.0-SNAPSHOT-jar-with-dependencies.jar
+	fi
+else
+		echo "Arquivo .jar já adquirido!"
+		echo "Executando arquivo"
+		cd JAR/gerencie/target
+		java -jar gerencie-1.0-SNAPSHOT-jar-with-dependencies.jar
+fi
